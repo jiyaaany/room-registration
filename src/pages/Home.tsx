5ee5b35542c962ad1150005af42e5b7bec570e66
jiyaaany <jiyaaany@gmail.com>
2021-06-17 20:53:49 +0900

    [feat] 홈에서 방 등록/목록 페이지로 이동

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Card, CardDeck } from 'react-bootstrap';
+import { AiOutlineHome } from 'react-icons/ai';
+import { FaRegListAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
   return (
-    <div>Home!</div>
+    <CardDeck className="text-center d-flex align-items-center min-vh-100">
+      <Card border="success" className="register-card py-5">
+        <Link to="/room/register" className="no-deco-link">
+          <Card.Body>
+            <Card.Title><AiOutlineHome /> 방 등록하기</Card.Title>
+            <Card.Text>
+              방의 정보를 입력해 방을 등록합니다.
+            </Card.Text>
+          </Card.Body>
+        </Link>
+      </Card>
+      <Card border="warning" className="list-card py-5">
+        <Link to="/rooms" className="no-deco-link">
+          <Card.Body>
+            <Card.Title><FaRegListAlt /> 방 목록으로 가기</Card.Title>
+            <Card.Text>
+              등록되어 있는 방의 목록 페이지로 이동합니다.
+            </Card.Text>
+          </Card.Body>
+        </Link>
+      </Card>
+    </CardDeck>
   );
 }
 
